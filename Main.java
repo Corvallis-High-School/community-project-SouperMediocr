875d4994a7f0d2d55868ffb27bca8c9ff45c50b8
Brandon Overbey <153847607+SouperMediocr@users.noreply.github.com>
2026-03-30 15:46:20 +0000

    changed main to call the methods of the class McDonaldsMenu as well as its subclasses

--- Main.java
+++ Main.java
@@ -5,12 +5,39 @@ class Main {
      Combo item2 = new Combo ("Cheeseburger Combo", 11.20, 24, 1300, 3);
      Drink item3 = new Drink ("Coca Cola", 2.0, 16.0, 350, "Medium");
 
-    System.out.println(item2.getCalories());
-    item.setCalories(400);
     System.out.println(item.getCalories());
-    item2.discountedItem(50);
+    System.out.println(item.getName());
+    System.out.println(item.getPrice());
+    System.out.println(item.getWeight());
+    item.setCalories(100);
+    item.setName("boragar");
+    item.setPrice(9999);
+    item.setWeight(5);
+    item.discountedItem(50);
+    
+    System.out.println(item2.getCalories());
+    System.out.println(item2.getName());
+    System.out.println(item2.getPrice());
+    System.out.println(item2.getWeight());
+    item2.setCalories(1000);
+    item2.setName("numba 7");
+    item2.setPrice(9);
+    item2.setWeight(500);
+    item2.discountedItem(12);
+    System.out.println(item2.getItemsInCombo());
+
+    System.out.println(item3.getCalories());
+    System.out.println(item3.getName());
+    System.out.println(item3.getPrice());
+    System.out.println(item3.getWeight());
+    item3.setCalories(4530);
+    item3.setName("peppi");
+    item3.setPrice(1);
+    item3.setWeight(50);
+    item3.discountedItem(40);
     System.out.println(item3.getSize());
-    System.out.println(item3.toString());
+    item3.setSize("small");
+
 
   }
 }
